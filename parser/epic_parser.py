233278fac3aeb7e3190fe6047757ddc3126477c5
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-10-26 03:41:12 +0300

    change epic parser to class add AbstractParser

--- parser/epic_parser.py
+++ parser/epic_parser.py
@@ -1,47 +1,45 @@
 import json
-
+import model
 import requests
 from bs4 import BeautifulSoup
 from pyxtension.streams import stream
-import model
-
-url = "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=BY" \
-      "&allowCountries=BY"
-__all__ = ['parse']
-
-def parse() -> list:
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.content, "html.parser")
-        data = json.loads(soup.prettify())
-        return stream(data.get('data').get('Catalog').get('searchStore').get('elements')).map(__get_game).toList()
-    except Exception as e:
-        print(e)
+from .AbstractParser import AbstractParser
 
 
-def __get_game(elements: dict) -> model.Game:
-    dates = __get_dates(elements)
-    return model.Game(title=elements.get('title'),
-                      description=elements.get('description'),
-                      original_price=elements.get('price').get('totalPrice').get('fmtPrice').get('originalPrice'),
-                      img=__find_thumbnail_img(elements.get('keyImages')),
-                      from_date=dates.get('startDate'),
-                      to_date=dates.get('endDate')
-                      )
+class EpicParser(AbstractParser):
+    __URL = "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=BY" \
+          "&allowCountries=BY"
 
+    def parse(self) -> list:
+        try:
+            response = requests.get(self.__URL)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.content, "html.parser")
+            data = json.loads(soup.prettify())
+            return stream(data.get('data').get('Catalog').get('searchStore').get('elements')).map(self.__get_game).toList()
+        except Exception as e:
+            print(e)
 
-def __find_thumbnail_img(elements: list) -> str:
-    return stream(elements) \
-        .filter(lambda element: element.get('type') == 'OfferImageWide') \
-        .map(lambda element: element.get('url')) \
-        .toList()[0]
+    def __get_game(self, elements: dict) -> model.Game:
+        dates = self.__get_dates(elements)
+        return model.Game(title=elements.get('title'),
+                          description=elements.get('description'),
+                          original_price=elements.get('price').get('totalPrice').get('fmtPrice').get('originalPrice'),
+                          img=self.__find_thumbnail_img(elements.get('keyImages')),
+                          from_date=dates.get('startDate'),
+                          to_date=dates.get('endDate')
+                          )
 
+    def __find_thumbnail_img(self, elements: list) -> str:
+        return stream(elements) \
+            .filter(lambda element: element.get('type') == 'OfferImageWide') \
+            .map(lambda element: element.get('url')) \
+            .toList()[0]
 
-def __get_dates(elements: dict) -> dict:
-    promotions = elements.get('promotions')
-    if promotions is None:
-        return {'startDate': '0', 'endDate': '0'}
-    elif not promotions.get('upcomingPromotionalOffers'):
-        return promotions.get('promotionalOffers')[0].get('promotionalOffers')[0]
-    return promotions.get('upcomingPromotionalOffers')[0].get('promotionalOffers')[0]
+    def __get_dates(self, elements: dict) -> dict:
+        promotions = elements.get('promotions')
+        if promotions is None:
+            return {'startDate': '0', 'endDate': '0'}
+        elif not promotions.get('upcomingPromotionalOffers'):
+            return promotions.get('promotionalOffers')[0].get('promotionalOffers')[0]
+        return promotions.get('upcomingPromotionalOffers')[0].get('promotionalOffers')[0]
